ee6e920ce7ae38680db631bf805afeefbf9c0b8e
ARTUR OLIVEIRA SANTIAGO <artur.santiago@SEFAZ.PE.GOV.BR>
2019-09-30 17:15:48 -0300

    Feature - Create Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,89 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
-    return <h1>Home</h1>;
+    return (
+        <ProductList>
+            <li>
+                <img src="" alt="Tênis" />
+                <strong>Tênis</strong>
+                <span>R$ 120,00</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img src="" alt="Tênis" />
+                <strong>Tênis</strong>
+                <span>R$ 120,00</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img src="" alt="Tênis" />
+                <strong>Tênis</strong>
+                <span>R$ 120,00</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img src="" alt="Tênis" />
+                <strong>Tênis</strong>
+                <span>R$ 120,00</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img src="" alt="Tênis" />
+                <strong>Tênis</strong>
+                <span>R$ 120,00</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+            <li>
+                <img src="" alt="Tênis" />
+                <strong>Tênis</strong>
+                <span>R$ 120,00</span>
+
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+        </ProductList>
+    );
 }
