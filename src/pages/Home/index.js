ab75aadfd40f9d24ae2ddbd336f55a3088fb3e52
ARTUR OLIVEIRA SANTIAGO <artur.santiago@SEFAZ.PE.GOV.BR>
2019-10-03 11:49:30 -0300

    Feature - Get Products from api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,89 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MdAddShoppingCart } from 'react-icons/md';
 
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
 import { ProductList } from './styles';
 
 export default function Home() {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function loadProducts() {
+            const response = await api.get('/products');
+
+            const data = response.data.map(product => ({
+                ...product,
+                priceFormatted: formatPrice(product.price),
+            }));
+            setProducts(data);
+        }
+        loadProducts();
+    });
+
     return (
         <ProductList>
-            <li>
-                <img src="" alt="Tênis" />
-                <strong>Tênis</strong>
-                <span>R$ 120,00</span>
+            {products.map(product => (
+                <li key={product.id}>
+                    <img src={product.image} alt={product.title} />
+                    <strong>{product.title}</strong>
+                    <span>{product.priceFormatted}</span>
 
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
+                    <button type="button">
+                        <div>
+                            <MdAddShoppingCart size={16} color="#FFF" /> 3
+                        </div>
 
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img src="" alt="Tênis" />
-                <strong>Tênis</strong>
-                <span>R$ 120,00</span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
-
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img src="" alt="Tênis" />
-                <strong>Tênis</strong>
-                <span>R$ 120,00</span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
-
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img src="" alt="Tênis" />
-                <strong>Tênis</strong>
-                <span>R$ 120,00</span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
-
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img src="" alt="Tênis" />
-                <strong>Tênis</strong>
-                <span>R$ 120,00</span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
-
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-            <li>
-                <img src="" alt="Tênis" />
-                <strong>Tênis</strong>
-                <span>R$ 120,00</span>
-
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#FFF" /> 3
-                    </div>
-
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
+                        <span>ADICIONAR AO CARRINHO</span>
+                    </button>
+                </li>
+            ))}
         </ProductList>
     );
 }
